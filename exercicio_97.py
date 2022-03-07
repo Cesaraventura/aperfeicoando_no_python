67ba4e5f35718cd7f30da467a528fe71a2ebe4a6
César Augusto Ventura <cesar.ventura.lima@gmail.com>
2022-03-07 11:07:29 -0300

    exercicio_97

---
+++ exercicio_97.py
@@ -0,0 +1,11 @@
+# exercicio 97
+def escreva(msg):
+    tam = len(msg) + 4
+    print('~' * tam)
+    print(f'  {msg}')
+    print('~' * tam)
+
+
+# programa principal
+escreva('Cesar Augusto Ventura')
+escreva('People Analytics')
